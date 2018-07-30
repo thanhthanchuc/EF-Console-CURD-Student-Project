eb1a52323857bd192dec34d2981a84edf57ae5d8
toiye <toiye@DESKTOP-BJB9GPG>
2018-07-30 11:26:53 +0700

    Add project files.

---
+++ EF Console CURD Student Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Console_CURD_Student_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
